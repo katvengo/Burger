3fa63c6e3233e5ecbe252b470b2f32f7d00b2637
Kat Vengo <katvengo@gmail.com>
2019-05-25 16:26:45 -0700

    file system

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id AUTO_INCREMENT,
+burger_name VARCHAR (255),
+devoured BOOLEAN,
+PRIMARY KEY (id)
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double-Double', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Texas-Burger', TRUE);
+
+
